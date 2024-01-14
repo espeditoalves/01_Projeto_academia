df9e03e5d7e237b150cc2f9ed41e8ced7f3d22ea
Espedito Ferreira Alves <espeditoalves123@hotmail.com>
2024-01-14 00:52:27 -0300

    Função envia_email_outlook definida e pronta na sua primeira versão

--- enviar_email.py
+++ enviar_email.py
@@ -1,6 +1,7 @@
 import win32com.client as win32
+from functions import *
 
-def enviar_email_outlook(destinatario, assunto, corpo):
+def enviar_email_outlook(destinatarios, assunto, dataframe_anexo, imagem1, imagem2):
     # Criar a integração com o Outlook
     outlook = win32.Dispatch('outlook.application')
 
@@ -8,23 +9,34 @@ def enviar_email_outlook(destinatario, assunto, corpo):
     email = outlook.CreateItem(0)
 
     # Configurar as informações do seu e-mail
-    email.To = destinatario
+    email.To = ";".join(destinatarios)  # Junta os destinatários com ponto e vírgula
     email.Subject = assunto
-    email.HTMLBody = corpo
+    
+    # Começo do e-mail
+    corpo_email = f"""
+    <p>Olá Janaina e Espedito!!</p>
+    <p>Segue para o conhecimento de vocês os resultados de treino da semana.</p>
+    """
+    # Adicionar o DataFrame ao corpo do e-mail
+    corpo_email += dataframe_anexo.to_html(index=False)
+    # Adiciona o Desfecho
+    corpo_email +=f"""
+    <p>Abs,</p>
+    <p>Desenvolvedor: Espedito Ferreira Alves</p>
+    """
+    email.HTMLBody = corpo_email
 
+    # Adicionar imagens como anexos
+    email.Attachments.Add(imagem1)
+    email.Attachments.Add(imagem2)
     # Enviar o e-mail
     email.Send()
     print("Email Enviado")
 
-# Exemplo de uso da função
-if __name__ == "__main__":
-    destinatario = "espeditoa100@gmail.com"
-    assunto = "Projeto Maromba - Resultados da Semana"
-    corpo = f"""
-    <p>Olá Janaina e Espedito!!</p>
-    <p>Segue para o conhecimento de vocês os resultados de treino da semana.</p>
+destinatarios = ["espeditoa100@gmail.com", "janainavdm@gmail.com"]
+assunto = "Projeto Treino Firme - Resultados da Semana"
+dataframe_anexo, data_formatada = analisa_excel()
+imagem1 = f'C:/Users/esped/OneDrive/0.Treinamentos e Cursos/0.Códigos e Scripts/2.Central de Conhecimentos/Projetos_pessoais/{data_formatada}_grafico_seaborn_Espedito.png'
+imagem2 = f'C:/Users/esped/OneDrive/0.Treinamentos e Cursos/0.Códigos e Scripts/2.Central de Conhecimentos/Projetos_pessoais/{data_formatada}_grafico_seaborn_Janaina.png'
 
-    <p>Abs,</p>
-    <p>Código Python</p>
-    """
-    enviar_email_outlook(destinatario, assunto, corpo)
+enviar_email_outlook(destinatarios, assunto, dataframe_anexo, imagem1, imagem2)
